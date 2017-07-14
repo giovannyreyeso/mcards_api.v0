3ff71463028c3969e35c1a934ab4e5902e2bbc64
Giovanny Reyes O <greyeso@concredito.com.mx>
2017-07-14 17:16:12 -0600

    Add mlab connection

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 const API_PORT = 3001
 const API_BASE_VERSION = '/v1'
 const DB_URL = 'mongodb://localhost:27017/mcards'
+const DB_URL_PRODUCTION = 'mongodb://giovannyreyeso:T00rCreeper89!?@ds157682.mlab.com:57682/mcards'
 module.exports = {
     API_PORT,
     API_BASE_VERSION,
-    DB_URL
+    DB_URL_PRODUCTION
 }
