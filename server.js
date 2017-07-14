ce4259829609e75aac5671a755f3478650ddfcd5
Giovanny Reyes O <greyeso@concredito.com.mx>
2017-07-14 17:21:37 -0600

    Add mlab connection

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const port = process.env.PORT || config.API_PORT
 const serviceAccount = require("./firebase/key.json");
-
-mongoose.connect(config.DB_URL, (err, res) => {
+   app.listen(port, () => {
+        console.log(`MCards API ready on port  ${port}`)
+    })
+/*mongoose.connect(config.DB_URL, (err, res) => {
     if (err)
         throw err
     console.log("Conexion a la base de datos establecida")
@@ -14,7 +16,5 @@ mongoose.connect(config.DB_URL, (err, res) => {
       credential: firebase_admin.credential.cert(serviceAccount),
       databaseURL: "https://mcards-c8e47.firebaseio.com"
     });
-    app.listen(port, () => {
-        console.log(`MCards API ready on port  ${port}`)
-    })
-})
+ 
+})*/
