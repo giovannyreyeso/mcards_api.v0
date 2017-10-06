9e807ddd5f723add96af6f6e0c9abee5985af911
creeper <greyeso@concredito.com.mx>
2017-10-06 14:00:49 -0600

    Deploy to heroku

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const port = process.env.PORT || config.API_PORT
 const serviceAccount = require("./firebase/key.json");
-   app.listen(port, () => {
-        console.log(`MCards API ready on port  ${port}`)
-    })
+app.listen(port, () => {
+    console.log(`MCards API ready on port  ${port}`)
+})
 mongoose.connect(config.DB_URL_PRODUCTION, (err, res) => {
     if (err)
         throw err
     console.log("Conexion a la base de datos establecida")
     firebase_admin.initializeApp({
       credential: firebase_admin.credential.cert(serviceAccount),
-      databaseURL: "https://mcards-c8e47.firebaseio.com"
+      databaseURL: "https://mcards-7449e.firebaseapp.com"
     });
  
 })
